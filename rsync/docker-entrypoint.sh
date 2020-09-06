b63bbd53e54555c13a7884b83fb60cf50fde0307
Arif Widi Nugroho <arif@sainsmograf.com>
2020-09-07 00:45:40 +0700

    rsync is now configured to sync files deletion

--- rsync/docker-entrypoint.sh
+++ rsync/docker-entrypoint.sh
@@ -8,7 +8,7 @@ else
     if [ -z "$PRIVATE_KEY" ]
     then
         echo "syncing files to $TARGET"
-        exec /usr/bin/rsync -e "ssh -o StrictHostKeyChecking=no" -avzh /source/ $TARGET --ignore-errors
+        exec /usr/bin/rsync -e "ssh -o StrictHostKeyChecking=no" -avzh /source/ $TARGET --delete --ignore-errors
         echo "done"
     else
         echo "$PRIVATE_KEY" > /home/.ssh-private-key
@@ -16,7 +16,7 @@ else
         echo "using PRIVATE_KEY"
 
         echo "syncing files to $TARGET"
-        exec /usr/bin/rsync -e "ssh -o StrictHostKeyChecking=no -i /home/.ssh-private-key" -avzh /source/ $TARGET --ignore-errors
+        exec /usr/bin/rsync -e "ssh -o StrictHostKeyChecking=no -i /home/.ssh-private-key" -avzh /source/ $TARGET --delete --ignore-errors
         echo "done"
     fi
 fi
