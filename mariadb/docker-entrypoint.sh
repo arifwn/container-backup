f3082f90c7d7db24271d0e9139c57d4ed0e49210
Arif Widi Nugroho <arif@sainsmograf.com>
2023-12-18 15:49:52 +0700

    updated mysql backup to backup stored procedures

--- mariadb/docker-entrypoint.sh
+++ mariadb/docker-entrypoint.sh
@@ -10,14 +10,14 @@ else
         databases=`mysql --host=$HOST --port=$PORT -u$USER -p$PASSWORD -e "SHOW DATABASES;"`
         for db in $databases; do
             if [[ "$db" != "information_schema" ]] && [[ "$db" != "performance_schema" ]] && [[ "$db" != "mysql" ]] && [[ "$db" != "sys" ]] && [[ "$db" != "Database" ]] ; then
-                    echo "running mysqldump --max_allowed_packet=1073741824 --host $HOST --port $PORT -u$USER -p$PASSWORD $db > $db.sql"
-                    mysqldump --max_allowed_packet=1073741824 --host $HOST --port $PORT -u$USER -p$PASSWORD $db | gzip -c > $db.sql
+                    echo "running mysqldump --max_allowed_packet=1073741824 --routines --host $HOST --port $PORT -u$USER -p$PASSWORD $db > $db.sql"
+                    mysqldump --max_allowed_packet=1073741824 --routines --host $HOST --port $PORT -u$USER -p$PASSWORD $db | gzip -c > $db.sql
             fi
         done
 
     else
-        echo "running mysqldump --max_allowed_packet=1073741824 --host $HOST --port $PORT -u$USER -p$PASSWORD $DBNAME > $DBNAME.sql"
-        exec mysqldump --max_allowed_packet=1073741824 --host $HOST --port $PORT -u$USER -p$PASSWORD $DBNAME | gzip -c > $DBNAME.sql
+        echo "running mysqldump --max_allowed_packet=1073741824 --routines --host $HOST --port $PORT -u$USER -p$PASSWORD $DBNAME > $DBNAME.sql"
+        exec mysqldump --max_allowed_packet=1073741824 --routines --host $HOST --port $PORT -u$USER -p$PASSWORD $DBNAME | gzip -c > $DBNAME.sql
         echo "done"
     fi
 fi
--- mysql-8.0/docker-entrypoint.sh
+++ mysql-8.0/docker-entrypoint.sh
@@ -10,14 +10,14 @@ else
         databases=`mysql --host=$HOST --port=$PORT -u$USER -p$PASSWORD -e "SHOW DATABASES;"`
         for db in $databases; do
             if [[ "$db" != "information_schema" ]] && [[ "$db" != "performance_schema" ]] && [[ "$db" != "mysql" ]] && [[ "$db" != "sys" ]] && [[ "$db" != "Database" ]] ; then
-                    echo "running mysqldump --max_allowed_packet=1073741824 --host $HOST --port $PORT -u$USER -p$PASSWORD $db > $db.sql"
-                    mysqldump --max_allowed_packet=1073741824 --host $HOST --port $PORT -u$USER -p$PASSWORD $db | gzip -c > $db.sql.gz
+                    echo "running mysqldump --max_allowed_packet=1073741824 --routines --host $HOST --port $PORT -u$USER -p$PASSWORD $db > $db.sql"
+                    mysqldump --max_allowed_packet=1073741824 --routines --host $HOST --port $PORT -u$USER -p$PASSWORD $db | gzip -c > $db.sql.gz
             fi
         done
 
     else
-        echo "running mysqldump --max_allowed_packet=1073741824 --host $HOST --port $PORT -u$USER -p$PASSWORD $DBNAME > $DBNAME.sql"
-        exec mysqldump --max_allowed_packet=1073741824 --host $HOST --port $PORT -u$USER -p$PASSWORD $DBNAME | gzip -c > $DBNAME.sql.gz
+        echo "running mysqldump --max_allowed_packet=1073741824 --routines --host $HOST --port $PORT -u$USER -p$PASSWORD $DBNAME > $DBNAME.sql"
+        exec mysqldump --max_allowed_packet=1073741824 --routines --host $HOST --port $PORT -u$USER -p$PASSWORD $DBNAME | gzip -c > $DBNAME.sql.gz
         echo "done"
     fi
 fi
