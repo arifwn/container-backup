60b4d59337694c9d4a56c51fc1fd0b3dd7e0b892
Arif Widi Nugroho <arif@sainsmograf.com>
2026-03-07 06:54:09 +0700

    updated mysql-8.0 backup container

--- mysql-8.0/docker-entrypoint.sh
+++ mysql-8.0/docker-entrypoint.sh
@@ -6,6 +6,11 @@ if [[ "$USE_SINGLE_TRANSACTION" == "true" || "$USE_SINGLE_TRANSACTION" == "1" ||
     SINGLE_TRANSACTION_OPT="--single-transaction"
 fi
 
+SOURCE_DATA_OPT="--master-data=2"
+if [[ "$USE_SOURCE_DATA" == "true" || "$USE_SOURCE_DATA" == "1" || "$USE_SOURCE_DATA" == "yes" ]]; then
+    SOURCE_DATA_OPT="--source-data=2"
+fi
+
 if [ -z "$DBNAME" ]
 then
     exec "$@"
@@ -15,8 +20,8 @@ else
         databases=`mysql --host=$HOST --port=$PORT -u$USER -p$PASSWORD -e "SHOW DATABASES;"`
         for db in $databases; do
             if [[ "$db" != "information_schema" ]] && [[ "$db" != "performance_schema" ]] && [[ "$db" != "mysql" ]] && [[ "$db" != "sys" ]] && [[ "$db" != "Database" ]] ; then
-                    echo "running mysqldump --max_allowed_packet=1073741824 $SINGLE_TRANSACTION_OPT --routines --triggers --events --master-data=2 --host $HOST --port $PORT -u$USER -p$PASSWORD $db > $db.sql"
-                    mysqldump --max_allowed_packet=1073741824 $SINGLE_TRANSACTION_OPT --routines --triggers --events --master-data=2 --host $HOST --port $PORT -u$USER -p$PASSWORD $db | gzip -c > $db.sql.gz
+                    echo "running mysqldump --max_allowed_packet=1073741824 $SINGLE_TRANSACTION_OPT --routines --triggers --events $SOURCE_DATA_OPT --host $HOST --port $PORT -u$USER -p$PASSWORD $db > $db.sql"
+                    mysqldump --max_allowed_packet=1073741824 $SINGLE_TRANSACTION_OPT --routines --triggers --events $SOURCE_DATA_OPT --host $HOST --port $PORT -u$USER -p$PASSWORD $db | gzip -c > $db.sql.gz
 
                     if [ -z "$SLEEP_DELAY" ]
                     then
@@ -29,8 +34,8 @@ else
         done
 
     else
-        echo "running mysqldump --max_allowed_packet=1073741824 $SINGLE_TRANSACTION_OPT --routines --triggers --events --master-data=2 --host $HOST --port $PORT -u$USER -p$PASSWORD $DBNAME > $DBNAME.sql"
-        exec mysqldump --max_allowed_packet=1073741824 $SINGLE_TRANSACTION_OPT --routines --triggers --events --master-data=2 --host $HOST --port $PORT -u$USER -p$PASSWORD $DBNAME | gzip -c > $DBNAME.sql.gz
+        echo "running mysqldump --max_allowed_packet=1073741824 $SINGLE_TRANSACTION_OPT --routines --triggers --events $SOURCE_DATA_OPT --host $HOST --port $PORT -u$USER -p$PASSWORD $DBNAME > $DBNAME.sql"
+        exec mysqldump --max_allowed_packet=1073741824 $SINGLE_TRANSACTION_OPT --routines --triggers --events $SOURCE_DATA_OPT --host $HOST --port $PORT -u$USER -p$PASSWORD $DBNAME | gzip -c > $DBNAME.sql.gz
         echo "done"
     fi
 fi
