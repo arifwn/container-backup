c00e66714b6becfb0fc5fe1f7aaf509938ad2b72
Arif Widi Nugroho <arif@sainsmograf.com>
2026-03-07 06:47:24 +0700

    updated mysql-8.0 backup container

--- mysql-8.0/docker-entrypoint.sh
+++ mysql-8.0/docker-entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 
+SINGLE_TRANSACTION_OPT=""
+if [[ "$USE_SINGLE_TRANSACTION" == "true" || "$USE_SINGLE_TRANSACTION" == "1" || "$USE_SINGLE_TRANSACTION" == "yes" ]]; then
+    SINGLE_TRANSACTION_OPT="--single-transaction"
+fi
+
 if [ -z "$DBNAME" ]
 then
     exec "$@"
@@ -10,8 +15,8 @@ else
         databases=`mysql --host=$HOST --port=$PORT -u$USER -p$PASSWORD -e "SHOW DATABASES;"`
         for db in $databases; do
             if [[ "$db" != "information_schema" ]] && [[ "$db" != "performance_schema" ]] && [[ "$db" != "mysql" ]] && [[ "$db" != "sys" ]] && [[ "$db" != "Database" ]] ; then
-                    echo "running mysqldump --max_allowed_packet=1073741824 --routines --host $HOST --port $PORT -u$USER -p$PASSWORD $db > $db.sql"
-                    mysqldump --max_allowed_packet=1073741824 --routines --host $HOST --port $PORT -u$USER -p$PASSWORD $db | gzip -c > $db.sql.gz
+                    echo "running mysqldump --max_allowed_packet=1073741824 $SINGLE_TRANSACTION_OPT --routines --triggers --events --master-data=2 --host $HOST --port $PORT -u$USER -p$PASSWORD $db > $db.sql"
+                    mysqldump --max_allowed_packet=1073741824 $SINGLE_TRANSACTION_OPT --routines --triggers --events --master-data=2 --host $HOST --port $PORT -u$USER -p$PASSWORD $db | gzip -c > $db.sql.gz
 
                     if [ -z "$SLEEP_DELAY" ]
                     then
@@ -24,8 +29,8 @@ else
         done
 
     else
-        echo "running mysqldump --max_allowed_packet=1073741824 --routines --host $HOST --port $PORT -u$USER -p$PASSWORD $DBNAME > $DBNAME.sql"
-        exec mysqldump --max_allowed_packet=1073741824 --routines --host $HOST --port $PORT -u$USER -p$PASSWORD $DBNAME | gzip -c > $DBNAME.sql.gz
+        echo "running mysqldump --max_allowed_packet=1073741824 $SINGLE_TRANSACTION_OPT --routines --triggers --events --master-data=2 --host $HOST --port $PORT -u$USER -p$PASSWORD $DBNAME > $DBNAME.sql"
+        exec mysqldump --max_allowed_packet=1073741824 $SINGLE_TRANSACTION_OPT --routines --triggers --events --master-data=2 --host $HOST --port $PORT -u$USER -p$PASSWORD $DBNAME | gzip -c > $DBNAME.sql.gz
         echo "done"
     fi
 fi
